5ba98604134157438f9b1b7a130a246bac88157a
Spencer Haka <spencer.haka@gmail.com>
2023-08-09 19:02:30 -0600

    First attempt at longest substring problem

--- longest-substring.js
+++ longest-substring.js
@@ -7,5 +7,32 @@ Output: 3
 Explanation: The answer is "abc", with the length of 3.
 
 Example #2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
 
+Example #3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+
+var lengthOfLongestSubstring = function (s) {
+  let longest = ''
+
+  for (let i = 0; i < s.length; i++) {
+    const slice = s.slice(i)
+    for (let j = 0; j < slice.length; j++) {
+      const prev = slice.slice(0, j)
+      console.log(prev)
+      const current = slice[j]
+      console.log(current)
+      if (prev.includes(current)) {
+        longest = prev
+      }
+    }
+  }
+  console.log(longest)
+  return longest.length
+}
