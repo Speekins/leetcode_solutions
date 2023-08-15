5a5af109aa77a2fbff9cb7ccbe26fdcce2d6f603
Spencer Haka <spencer.haka@gmail.com>
2023-08-15 08:51:44 -0600

    Complete palindrome number problem

---
+++ palindromeNum.js
@@ -0,0 +1,22 @@
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+//Solution converting to string
+var isPalindrome = function (x) {
+  let opposite = 0
+
+  opposite = x % 10
+}
+
+//Solution not converting to string
+var isPalindrome2 = function (x) {
+  let x = 543
+  let total = 0
+
+  while (true) {
+    total = total * 10 + (x % 10)
+    x = Math.floor(x / 10)
+    if (x === 0) break
+  }
+
+  return total === x
+}
